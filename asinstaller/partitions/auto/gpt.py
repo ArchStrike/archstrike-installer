3b128b6fe0e8be5e8b8623981889c723536391e8
Craig West <cawest1221@gmail.com>
2017-01-04 17:36:46 -0500

    more consistant partitions

--- asinstaller/partitions/auto/gpt.py
+++ asinstaller/partitions/auto/gpt.py
@@ -8,9 +8,9 @@ def uefi():
         system('echo -e "n\n\n\n512M\nef00\nn\n3\n\n' \
             + '+{0}\n8200\nn\n\n'.format(usr_cfg['swap_space']) \
             + '\n\n\nw\ny" | gdisk {0}'.format(usr_cfg['drive']))
-        SWAP = system_output("lsblk | " \
-                    + "grep {0} | ".format(usr_cfg['drive'][-3:]) \
-                    + "awk '{ if (NR==4) print substr ($1,3) }' ")
+        SWAP = system_output("fdisk -l | " \
+                    + " grep {0}".format(usr_cfg['drive'][-3:]) \
+                    + " |  awk '{ if (NR==4) print substr ($1,6) }'")
         system("wipefs -afq /dev/{0}".format(SWAP))
         system("mkswap /dev/{0}".format(SWAP))
         system("swapon /dev/{0}".format(SWAP))
@@ -19,12 +19,12 @@ def uefi():
         system('echo -e "n\n\n\n512M\nef00\nn\n\n\n\n\nw\ny" | ' \
             + 'gdisk {0}'.format(drive))
 
-    usr_cfg['boot'] = system_output("lsblk | " \
-            + "grep {0} |  ".format(usr_cfg['drive'][-3:]) \
-            + "awk '{ if (NR==2) print substr ($1,3) }'")
-    usr_cfg['root'] = system_output("lsblk | " \
-            + "grep {0} |  ".format(usr_cfg['drive'][-3:]) \
-            + "awk '{ if (NR==3) print substr ($1,3) }'")
+    usr_cfg['boot'] = system_output("fdisk -l | " \
+                + "grep {0} | ".format(usr_cfg['drive'][-3:]) \
+                + "awk '{ if (NR==2) print substr ($1,6) }' ")
+    usr_cfg['root'] = system_output("fdisk -l | " \
+                + "grep {0} |  ".format(usr_cfg['drive'][-3:]) \
+                + "awk '{ if (NR==3) print substr ($1,6) }' ")
 
 
 def non_uefi():
@@ -32,9 +32,9 @@ def non_uefi():
         system('echo -e "o\ny\nn\n1\n\n+100M\n\nn\n2\n\n+1M\nEF02\nn\n4\n\n' \
             + '+{0}\n8200\nn'.format(usr_cfg['swap_space']) \
             + '\n3\n\n\n\nw\ny" | gdisk {0}'.format(usr_cfg['drive']))
-        SWAP = system_output("lsblk | " \
-                    + "grep {0} |  ".format(usr_cfg['drive'][-3:]) \
-                    + "awk '{ if (NR==5) print substr ($1,3) }' ")
+        SWAP = system_output("fdisk -l | " \
+                    + " grep {0}".format(usr_cfg['drive'][-3:]) \
+                    + " |  awk '{ if (NR==5) print substr ($1,6) }'")
         system("wipefs -afq /dev/{0}".format(SWAP))
         system("mkswap /dev/{0}".format(SWAP))
         system("swapon /dev/{0}".format(SWAP))
@@ -43,9 +43,9 @@ def non_uefi():
         system('echo -e "o\ny\nn\n1\n\n+100M\n\nn\n2\n\n+1M\nEF02\nn' \
             + '\n3\n\n\n\nw\ny" | gdisk {0}'.format(usr_cfg['drive']))
 
-    usr_cfg['boot'] = system_output("lsblk | " \
-            + "grep {0} |  ".format(usr_cfg['drive'][-3:]) \
-            + "awk '{ if (NR==2) print substr ($1,3) }'")
-    usr_cfg['root'] = system_output("lsblk | " \
-            + "grep {0} |  ".format(usr_cfg['drive'][-3:]) \
-            + "awk '{ if (NR==4) print substr ($1,3) }'")
+    usr_cfg['boot'] = system_output("fdisk -l | " \
+                + "grep {0} | ".format(usr_cfg['drive'][-3:]) \
+                + "awk '{ if (NR==2) print substr ($1,6) }' ")
+    usr_cfg['root'] = system_output("fdisk -l | " \
+                + "grep {0} |  ".format(usr_cfg['drive'][-3:]) \
+                + "awk '{ if (NR==3) print substr ($1,6) }' ")
