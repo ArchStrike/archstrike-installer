f97bfb736b3cf0d0eb884fb7f6199f082b2dcadb
James Stronz <j.a.stronz@gmail.com>
2019-03-15 23:37:41 -0500

    Style improvements and undef fixed for manual.utils

--- asinstaller/partitions/manual/utils.py
+++ asinstaller/partitions/manual/utils.py
@@ -1,16 +1,18 @@
+import logging
 import time
-from asinstaller.utils import *
+from asinstaller.utils import print_error, print_warning, print_title, print_info,\
+    cinput, query_yes_no, system, satisfy_dep
+from config import COLORS
 from asinstaller.config import usr_cfg, setup_logger
+
 
 logger = setup_logger(__name__)
 
 
 def format_partition_type(i):
     print_warning("Partition {0} will be formatted now".format(i))
-    partition_type = cinput('> Enter the partition type ' \
-        + '(linux, uefi, swap): ', COLORS['OKBLUE']).lower()
-    logger.log(logging.INFO, 'Partition Type: ' \
-        + '{0}'.format(partition_type))
+    partition_type = cinput('> Enter the partition type (linux, uefi, swap): ', COLORS['OKBLUE']).lower()
+    logger.log(logging.INFO, 'Partition Type: {0}'.format(partition_type))
     if partition_type == 'linux':
         satisfy_dep("mkfs.ext4")
         system("mkfs.ext4 {0}".format(i))
@@ -33,9 +35,8 @@ def format_partition():
     time.sleep(1)
 
     system("lsblk {}".format(usr_cfg['drive']))
-    partitions = cinput('> Enter all the partitions you created by ' \
-        + 'seperating them with a comma (e.g. /dev/sda1,/dev/sda2): ' \
-        , COLORS['OKBLUE']).split(',')
+    _in_prompt = '> Enter all the partitions you created by seperating them with a comma (e.g. /dev/sda1,/dev/sda2): '
+    partitions = cinput(_in_prompt, COLORS['OKBLUE']).split(',')
 
     logger.log(logging.INFO, "System Partitions: {0}".format(partitions))
 
@@ -55,7 +56,7 @@ def mount():
 
     print_info('\n'.join(usr_cfg['manual_partitions']))
     usr_cfg['root'] = cinput('Which one is your / mounted partition? (e.g. /dev/sda1): ',
-            COLORS['OKBLUE'])
+                             COLORS['OKBLUE'])
 
     logger.log(logging.INFO, "/ is {0}".format(usr_cfg['root']))
     if usr_cfg['root'] in usr_cfg['manual_partitions']:
@@ -68,7 +69,7 @@ def mount():
         _in_prompt = 'Which one is your /boot mounted partition? (e.g. /dev/sda2): '
         usr_cfg['boot'] = cinput(_in_prompt, COLORS['OKBLUE'])
         logger.log(logging.INFO, "/boot is {0}".format(usr_cfg['boot']))
-        if usr_cfg['boot'] in partitions:
+        if usr_cfg['boot'] in usr_cfg['manual_partitions']:
             print_info("Mounting {0} on /mnt/boot".format(usr_cfg['boot']))
             system("mkdir -p /mnt/boot")
             system("mount {0} /mnt/boot".format(usr_cfg['boot']))
