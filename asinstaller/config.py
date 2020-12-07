9d09b8497e6672bb363f907b9114fd54f9b03507
comrumino <comrumino@archstrike.org>
2020-12-06 23:48:50 -0600

    removed shandle from init_logger_handlers as test responsibilities should be isolated to test

--- asinstaller/config.py
+++ asinstaller/config.py
@@ -1,4 +1,3 @@
-import io
 import json
 import logging
 import os
@@ -7,7 +6,6 @@ from pathlib import Path
 __all__ = ["pacmanconf", "archstrike_mirrorlist", "CRASH_FILE", "LOG_FILE", "CONFIG_FILE", "IRC_SERVER", "IRC_PORT",
            "IRC_BOT_NICK", "COLORS", "localesdict", "FNULL", "TOPLEVEL_NAME", "usr_cfg", "WhitespaceRemovingFormatter",
            "init_logger_handles", "get_logger", "load_user_cfg", "dump_user_cfg"]
-
 
 
 pacmanconf = "/etc/pacman.conf"
@@ -51,36 +49,31 @@ class WhitespaceRemovingFormatter(logging.Formatter):
 def init_logger_handles():
     """scopes loggers by package.filename, scope collision is an accepted flaw"""
     FHANDLE_NAME = 'Installer Log'
-    SHANDLE_NAME = 'Unit Test String Buffer'
     root_logger = logging.getLogger()
     root_logger.setLevel(logging.NOTSET)
     top_logger = logging.getLogger(TOPLEVEL_NAME)
-    if all([hndlr.name not in set([FHANDLE_NAME, SHANDLE_NAME]) for hndlr in top_logger.handlers]):
+    if all([hndlr.name != FHANDLE_NAME for hndlr in top_logger.handlers]):
         top_logger.setLevel(logging.DEBUG)  # NOTSET only traverses until another level is found, so DEBUG is preferred
         # add a file handler to LOG_FILE, handle for standard io (stdio_handle), and a handle for units (shandle)
         fhandle = logging.FileHandler(LOG_FILE, mode='a+')
         fhandle.name = FHANDLE_NAME
         fhandle.setLevel(logging.DEBUG)
-        shandle = logging.StreamHandler(io.StringIO())
-        shandle.name = SHANDLE_NAME
-        shandle.setLevel(logging.DEBUG)
         stdio_handle = logging.StreamHandler()
         stdio_handle.setLevel(logging.INFO)
         fmt = logging.Formatter(fmt='%(levelname)s - %(message)s', datefmt='%m-%d %H:%M')
         fhandle.setFormatter(fmt)
-        shandle.setFormatter(fmt)
         stdio_handle.setFormatter(fmt)
         top_logger.addHandler(fhandle)
-        top_logger.addHandler(shandle)
         top_logger.addHandler(stdio_handle)
 
 
-def get_logger(filename):
-    return logging.getLogger(f'{TOPLEVEL_NAME}.{filename}')
+def get_logger(filename=None):
+    return logging.getLogger(f'{TOPLEVEL_NAME}.{filename}' if filename is not None else f'{TOPLEVEL_NAME}')
 
 
 def load_user_cfg():
     try:
+        global usr_cfg
         if CONFIG_FILE.exists():
             with open(CONFIG_FILE) as fr:
                 usr_cfg = json.loads(fr)
@@ -97,10 +90,9 @@ def dump_user_cfg():
         logger = get_logger(__file__)
         if CONFIG_FILE.exists():
             with open(CONFIG_FILE) as fr:
-                usr_cfg = json.loads(fr)
-    except TypeError as exc:
-        import pdb; pdb.set_trace()
-        logger.warning(f"User config ")
+                json.dumps(fr, usr_cfg)
+    except TypeError:
+        logger.warning(f"User config {usr_cfg} failed to dump to {CONFIG_FILE}")
         return {}
     except Exception:
         raise
